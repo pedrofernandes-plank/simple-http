2a99e44fb9baab327fff7a7b18006582903c51e3
Pedro Fernandes <pedro.fernandes@joinplank.com>
2023-10-31 15:40:19 -0300

    remove comments

--- javascript/app.js
+++ javascript/app.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const app = express();
 
-// Endpoint to return HTML with "Hello, World" in an H1 element
 app.get('/hello', (req, res) => {
   const htmlResponse = '<html><body><h1>Hello, World!</h1></body></html>';
   res.send(htmlResponse);
 });
 
-// Endpoint to return JSON with random data
 app.get('/random', (req, res) => {
   const randomData = {
     data: Math.random(),
